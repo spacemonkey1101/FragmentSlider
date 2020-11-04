da5cad6b679976f048cd4dab08fac2d7b357530a
spacemonkey1101 <ritambh54@gmail.com>
2020-11-04 22:02:06 +0530

    create a fragment class

---
+++ app/src/main/java/com/example/fragmentslider/SimpleFragment.java
@@ -0,0 +1,8 @@
+package com.example.fragmentslider;
+
+import androidx.fragment.app.Fragment;
+
+public class SimpleFragment extends Fragment {
+    // Holds the fragment id passed in when created
+    public static final String MESSAGE = "";
+}
